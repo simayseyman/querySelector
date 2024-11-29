063a70fd37d413ea5297b25eef6c837a0acfe3f5
Simay Seyman <77544761+simayseyman@users.noreply.github.com>
2024-11-29 12:01:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const header = document.querySelector("#header");
+header.style.backgroundColor = "black";
+header.innerHTML = "SİMAY SEYMAN";
+header.style.border = "5px solid red";
+
+const pageTitle = document.querySelector("#page_title");
+pageTitle.style.backgroundColor = "black";
+pageTitle.innerHTML = "YETENEK BAŞAKŞEHİR";
+pageTitle.style.border = "5px solid purple";
+
+const content = document.querySelector("#content");
+content.style.backgroundColor = "black";
+content.innerHTML = "YB1 ÖDEV";
+content.style.border = "5px solid blue";
+
+const footer = document.querySelector("#footer");
+footer.style.backgroundColor = "black";
+footer.style.color = "yellow"
+footer.innerHTML = "29.11.2024";
+footer.style.border = "5px solid yellow";
+
